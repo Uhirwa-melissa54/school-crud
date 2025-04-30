fdd0ca52f8ea7a9e5c90e9b30dc43634b484eeb9
Shami Melissa <uhirwamelissa@gmail.com>
2025-04-29 23:17:28 -0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+# School Management CRUD App
+
+This is a Node.js and Express.js-based CRUD (Create, Read, Update, Delete) application designed for managing school data. It supports both:
+
+- **With database**: MongoDB (Mongoose)
+- **Without database**: In-memory JavaScript object (temporary data storage)
+
+## 🛠 Technologies Used
+
+- Node.js
+- Express.js
+- MongoDB with Mongoose (for persistent storage)
+  
+
+---
+
+
+
 
--- models/mongodb.js
+++ models/mongodb.js
@@ -1,29 +1,30 @@
-// blog_app/config/db.js
-
-const mongoose = require("mongoose")
-const dotenv=require('dotenv').config();
-
-function connectDB() {
-  const url=process.env.MONGO_URL;
-
-  try {
-    mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-  const dbConnection = mongoose.connection;
-  dbConnection.once("open", (_) => {
-    console.log(`Database connected: ${url}`);
-  });
-
-  dbConnection.on("error", (err) => {
-    console.error(`connection error: ${err}`);
-  });
-  return;
-}
-
-module.exports= connectDB
+// blog_app/config/db.js
+
+const mongoose = require("mongoose")
+const dotenv=require('dotenv').config();
+
+function connectDB() {
+  const url=process.env.MONGO_URL;
+
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    console.log(`Database connected: ${url}`);
+  });
+
+  dbConnection.on("error", (err) => {
+    console.error(`connection error: ${err}`);
+  });
+  return;
+}
+
+module.exports= connectDB;
+
